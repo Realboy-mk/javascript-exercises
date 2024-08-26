76964e497ca0637819dd2ab82282ed5d20b2c2bb
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-26 17:16:52 -0400

    fix: add variables with Math.round method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const convertToCelsius = function(tempInFar) {
-  tempInCelsius = (tempInFar - 32) *5 / 9;
-  return tempInCelsius;
+  tempCelsius = ((tempInFar - 32) * 5 / 9);
+  tempCelsius = Math.round(tempCelsius * 10) / 10;
+  return tempCelsius;
 };
 
 const convertToFahrenheit = function(tempInCel) {
-  tempinFahrenheit = (tempinCel *5 / 9) + 32;
-  return tempinFahrenheit;
+  tempFahrenheit = ((tempInCel * 9 / 5) + 32);
+  tempFahrenheit = Math.round(tempFahrenheit * 10) / 10;
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
