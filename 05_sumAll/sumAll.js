f0a9115c8af9594d7d616dc7c7f365163631431b
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-26 15:53:28 -0400

    fix: add function to sumAall with new paramters.
    added conditional statements to test if firstNum and lastNum are
    non-negative integers. Also added logic to reverse first and last
    numbers if they were entered out of order.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    if (firstNum > lastNum){
+        const x = firstNum;
+        firstNum = lastNum;
+        lastNum = x;
+    }
+    if (firstNum < 0 || lastNum < 0){
+        return 'ERROR';
+    } else if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)){
+        return 'ERROR';
+    }
+    let total = 0;
+        for (let i = firstNum; i <= lastNum; i++){
+        total += i;
+        }
+        return total;
 };
 
 // Do not edit below this line
