ade0f961ab9bc3e898c904415b16ed16a220b2b6
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-26 16:41:16 -0400

    fix: rewrite conditionals into variables
    add three variables to return boolean value to test year values.
    add if statement with && condtions assigned from variables.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,10 @@
 const leapYears = function(year) {
-    if (year % 4 == 0){
+    const isDivisibleByFour = year % 4 === 0;
+    const isCenturyYear = year % 100 === 0;
+    const isFourHundredYear = year % 400 === 0;
+    if (isDivisibleByFour && (!isCenturyYear || isFourHundredYear)){
         return true;
-    } else return false;
+    }else return false;
 };
 
 // Do not edit below this line
