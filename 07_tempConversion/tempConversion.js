21f1282017daceda65987d938ac7e162a0b2809c
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-26 16:55:14 -0400

    feat: add functions to convert C to F and F to C

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFar) {
+  tempInCelsius = (tempInFar - 32) *5 / 9;
+  return tempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  tempinFahrenheit = (tempinCel *5 / 9) + 32;
+  return tempinFahrenheit;
 };
 
 // Do not edit below this line
