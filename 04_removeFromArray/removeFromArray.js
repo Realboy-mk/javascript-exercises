249f6144b86293b6c1ba282bb8b57f4c4d84aa52
Realboy-mk <[matt.kline.code@gmail.com]>
2024-08-26 15:32:33 -0400

    fix: add and test function removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    const newArray = [];
+    array.forEach(element => {
+        if (!numbers.includes(element))
+            newArray.push(element);
+    });
+    return newArray;
 };
 
 // Do not edit below this line
